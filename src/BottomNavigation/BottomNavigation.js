d45126c00f53d6af7bb3d8f93b730d6fa4075dbc
titusefferian <kiritosato@gmail.com>
2019-01-12 14:57:36 +0700

    linting

--- src/BottomNavigation/BottomNavigation.js
+++ src/BottomNavigation/BottomNavigation.js
@@ -6,18 +6,20 @@ import Search from '../activity/Search';
 import MyProfile from '../activity/MyProfile';
 import { RED_COLOR } from '../styles';
 
-const BottomNavigation = createBottomTabNavigator({
-    Home: HomeScreen,
-    Favourite: Favourite,
-    Search: Search,
-    History: History,
-    MyProfile: MyProfile
-},
+const BottomNavigation = createBottomTabNavigator(
+    {
+        Home: HomeScreen,
+        Favourite: Favourite,
+        Search: Search,
+        History: History,
+        MyProfile: MyProfile
+    },
     {
         tabBarOptions: {
             showLabel: false,
             activeTintColor: RED_COLOR
         },
-    })
+    }
+)
 
 export default createAppContainer(BottomNavigation)
