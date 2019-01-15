cefcbb8e77882e383e5599bdc8a5a3084e6d8b7b
titusefferian <kiritosato@gmail.com>
2019-01-15 09:19:08 +0700

    commit

--- src/components/Widget.js
+++ src/components/Widget.js
@@ -24,6 +24,21 @@ class Widget extends React.PureComponent {
         </ImageBackground>
     );
 
+    _renderItemRestorant = ({ item, index }) => (
+        <ImageBackground
+            source={{ uri: 'https://www.qraved.com/jakarta/' + item.image }}
+            style={{ width: 236, height: 132, marginRight: index == this.props.data.length - 1 ? 0 : 8, borderRadius: 8, overflow: 'hidden', backgroundColor: this.state.onLoad ? GRAY_COLOR : null }}
+            imageStyle={{
+                backgroundColor: GRAY_COLOR
+            }}
+            onLoad={() => { this.setState({ onLoad: false }) }}
+        >   
+            <View style={{ flex: 1, backgroundColor: 'black', opacity: 0.2 }}>
+            </View>
+            <Text style={{ position: 'absolute', bottom: 10, left: 10, ...FONT_BOLD, color: 'white' }}>{item.title}</Text>
+        </ImageBackground>
+    );
+
     _renderItemNews = ({ item, index }) => (
         <View style={{flex:1}}>
 
@@ -68,6 +83,37 @@ class Widget extends React.PureComponent {
                             <View style={{ width: 155, height: 143, borderRadius: 8, backgroundColor: GRAY_COLOR, marginRight: 8 }}>
                             </View>
                             <View style={{ width: 155, height: 143, borderRadius: 8, backgroundColor: GRAY_COLOR, marginRight: 8 }}>
+                            </View>
+                        </View>
+                    )
+                    }
+                    keyExtractor={(item, index) => index.toString()}
+                ></FlatList>
+            </View>
+            )
+        }
+        if(this.props.isRestorant){
+            return(
+                <View style={styles.container}>
+                <View style={styles.title}>
+                    <Text style={{ ...FONT_BOLD, ...FONT_SIZE_LARGE }}>{this.props.leftLabel}</Text>
+                    <Text style={{ ...FONT_SIZE_NORMAL }}>{this.props.rightLabel}</Text>
+                </View>
+                <FlatList
+                    style={{ marginTop: 16 }}
+                    data={this.props.isLoading ? undefined : this.props.data}
+                    renderItem={this._renderItemRestorant}
+                    horizontal={true}
+                    showsHorizontalScrollIndicator={false}
+                    ListEmptyComponent={() => (
+                        <View style={{ flex: 1, flexDirection: 'row' }}>
+                            <View style={{ width: 236, height: 132, borderRadius: 8, backgroundColor: GRAY_COLOR, marginRight: 8 }}>
+                            </View>
+                            <View style={{ width: 236, height: 132, borderRadius: 8, backgroundColor: GRAY_COLOR, marginRight: 8 }}>
+                            </View>
+                            <View style={{ width: 236, height: 132, borderRadius: 8, backgroundColor: GRAY_COLOR, marginRight: 8 }}>
+                            </View>
+                            <View style={{ width: 236, height: 132, borderRadius: 8, backgroundColor: GRAY_COLOR, marginRight: 8 }}>
                             </View>
                         </View>
                     )
