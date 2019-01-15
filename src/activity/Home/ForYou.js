518478d35cfc6f1806356f87e444231530103836
titusefferian <kiritosato@gmail.com>
2019-01-15 09:04:41 +0700

    add widget for restorant

--- src/activity/Home/ForYou.js
+++ src/activity/Home/ForYou.js
@@ -18,33 +18,40 @@ class ForYou extends React.Component {
         return (
             <ScrollView style={{ flex: 1, backgroundColor: GRAY_COLOR }}>
                 {
-                    this.props.home.data!=undefined
-                    ?
-                    <Widget leftLabel='For You' rightLabel='show all' data={this.props.home.data.homeSections[0].data}></Widget>
-                    :
-                    <Widget leftLabel='For You' rightLabel='show all' isLoading={true}></Widget>
-                        
+                    this.props.home.data != undefined
+                        ?
+                        <Widget leftLabel='New Restaurants' rightLabel='show all' data={this.props.home.data.homeSections[2].data} isRestorant={true}></Widget>
+                        :
+                        <Widget leftLabel='New Restaurants' rightLabel='show all' isLoading={true}></Widget>
                 }
                 {
-                    this.props.home.data!=undefined
-                    ?
-                    <Widget leftLabel='Occasion' rightLabel='show all' data={this.props.home.data.homeSections[3].data}></Widget>
-                    :
-                    <Widget leftLabel='Occasion' rightLabel='show all' isLoading={true}></Widget>  
+                    this.props.home.data != undefined
+                        ?
+                        <Widget leftLabel='For You' rightLabel='show all' data={this.props.home.data.homeSections[0].data}></Widget>
+                        :
+                        <Widget leftLabel='For You' rightLabel='show all' isLoading={true}></Widget>
+
                 }
                 {
-                    this.props.home.data!=undefined
-                    ?
-                    <Widget leftLabel='Latest news' rightLabel='show all' data={this.props.home.data.homeSections[1].data} isNews={true}></Widget>
-                    :
-                    <Widget leftLabel='Latest news' rightLabel='show all' isLoading={true}></Widget>  
+                    this.props.home.data != undefined
+                        ?
+                        <Widget leftLabel='Occasion' rightLabel='show all' data={this.props.home.data.homeSections[3].data}></Widget>
+                        :
+                        <Widget leftLabel='Occasion' rightLabel='show all' isLoading={true}></Widget>
                 }
                 {
-                    this.props.home.data!=undefined
-                    ?
-                    <Widget leftLabel='Trending Article' rightLabel='show all' data={this.props.home.data.homeSections[4].data} isNews={true}></Widget>
-                    :
-                    <Widget leftLabel='Trending Article' rightLabel='show all' isLoading={true}></Widget>  
+                    this.props.home.data != undefined
+                        ?
+                        <Widget leftLabel='Latest news' rightLabel='show all' data={this.props.home.data.homeSections[1].data} isNews={true}></Widget>
+                        :
+                        <Widget leftLabel='Latest news' rightLabel='show all' isLoading={true}></Widget>
+                }
+                {
+                    this.props.home.data != undefined
+                        ?
+                        <Widget leftLabel='Trending Article' rightLabel='show all' data={this.props.home.data.homeSections[4].data} isNews={true}></Widget>
+                        :
+                        <Widget leftLabel='Trending Article' rightLabel='show all' isLoading={true}></Widget>
                 }
             </ScrollView>
         )
