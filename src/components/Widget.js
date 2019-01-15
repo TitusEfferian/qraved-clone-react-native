0745d966620e4145c86b86337f211f7f5a16d7d8
titusefferian <kiritosato@gmail.com>
2019-01-15 10:35:05 +0700

    remove listemptycomponent

--- src/components/Widget.js
+++ src/components/Widget.js
@@ -17,7 +17,7 @@ class Widget extends React.PureComponent {
                 backgroundColor: GRAY_COLOR
             }}
             onLoad={() => { this.setState({ onLoad: false }) }}
-        >   
+        >
             <View style={{ flex: 1, backgroundColor: 'black', opacity: 0.2 }}>
             </View>
             <Text style={{ position: 'absolute', bottom: 10, left: 10, ...FONT_BOLD, color: 'white' }}>{item.title}</Text>
@@ -32,95 +32,66 @@ class Widget extends React.PureComponent {
                 backgroundColor: GRAY_COLOR
             }}
             onLoad={() => { this.setState({ onLoad: false }) }}
-        >   
-            <View style={{ flex: 1, backgroundColor: 'black', opacity: 0.2 }}>
+        >
+            <View style={{ width: 236, height: 132, backgroundColor: 'black', opacity: 0.2 }}>
             </View>
             <Text style={{ position: 'absolute', bottom: 10, left: 10, ...FONT_BOLD, color: 'white' }}>{item.title}</Text>
         </ImageBackground>
     );
 
     _renderItemNews = ({ item, index }) => (
-        <View style={{flex:1}}>
-
-        
-        <ImageBackground
-            source={{ uri: 'https://www.qraved.com/jakarta/' + item.image }}
-            style={{ width: 155, height: 143, marginRight: index == this.props.data.length - 1 ? 0 : 8, borderRadius: 8, overflow: 'hidden',zIndex:1, backgroundColor: this.state.onLoad ? GRAY_COLOR : null }}
-            imageStyle={{
-                backgroundColor: GRAY_COLOR
-            }}
-            onLoad={() => { this.setState({ onLoad: false }) }}
-        >   
-            <View style={{ flex: 1, backgroundColor: 'black', opacity: 0.2 }}>
-            </View>
-        </ImageBackground>
-        <Text style={{   color: 'black',width:155 }}>{item.title}</Text>
+        <View style={{ flex: 1 }}>
+            <ImageBackground
+                source={{ uri: 'https://www.qraved.com/jakarta/' + item.image }}
+                style={{ width: 155, height: 143, marginRight: index == this.props.data.length - 1 ? 0 : 8, borderRadius: 8, overflow: 'hidden', zIndex: 1, backgroundColor: this.state.onLoad ? GRAY_COLOR : null }}
+                imageStyle={{
+                    backgroundColor: GRAY_COLOR
+                }}
+                onLoad={() => { this.setState({ onLoad: false }) }}
+            >
+                <View style={{ flex: 1, backgroundColor: 'black', opacity: 0.2 }}>
+                </View>
+            </ImageBackground>
+            <Text style={{ color: 'black', width: 155 }}>{item.title}</Text>
 
         </View>
     );
 
     render() {
-        if(this.props.isNews)
-        {
-            return(
+        if (this.props.isNews) {
+            return (
                 <View style={styles.container}>
-                <View style={styles.title}>
-                    <Text style={{ ...FONT_BOLD, ...FONT_SIZE_LARGE }}>{this.props.leftLabel}</Text>
-                    <Text style={{ ...FONT_SIZE_NORMAL }}>{this.props.rightLabel}</Text>
+                    <View style={styles.title}>
+                        <Text style={{ ...FONT_BOLD, ...FONT_SIZE_LARGE }}>{this.props.leftLabel}</Text>
+                        <Text style={{ ...FONT_SIZE_NORMAL }}>{this.props.rightLabel}</Text>
+                    </View>
+                    <FlatList
+                        style={{ marginTop: 16 }}
+                        data={this.props.isLoading ? undefined : this.props.data}
+                        renderItem={this._renderItemNews}
+                        horizontal={true}
+                        showsHorizontalScrollIndicator={false}
+                        keyExtractor={(item, index) => index.toString()}
+                    ></FlatList>
                 </View>
-                <FlatList
-                    style={{ marginTop: 16 }}
-                    data={this.props.isLoading ? undefined : this.props.data}
-                    renderItem={this._renderItemNews}
-                    horizontal={true}
-                    showsHorizontalScrollIndicator={false}
-                    ListEmptyComponent={() => (
-                        <View style={{ flex: 1, flexDirection: 'row' }}>
-                            <View style={{ width: 155, height: 143, borderRadius: 8, backgroundColor: GRAY_COLOR, marginRight: 8 }}>
-                            </View>
-                            <View style={{ width: 155, height: 143, borderRadius: 8, backgroundColor: GRAY_COLOR, marginRight: 8 }}>
-                            </View>
-                            <View style={{ width: 155, height: 143, borderRadius: 8, backgroundColor: GRAY_COLOR, marginRight: 8 }}>
-                            </View>
-                            <View style={{ width: 155, height: 143, borderRadius: 8, backgroundColor: GRAY_COLOR, marginRight: 8 }}>
-                            </View>
-                        </View>
-                    )
-                    }
-                    keyExtractor={(item, index) => index.toString()}
-                ></FlatList>
-            </View>
             )
         }
-        if(this.props.isRestorant){
-            return(
+        if (this.props.isRestorant) {
+            return (
                 <View style={styles.container}>
-                <View style={styles.title}>
-                    <Text style={{ ...FONT_BOLD, ...FONT_SIZE_LARGE }}>{this.props.leftLabel}</Text>
-                    <Text style={{ ...FONT_SIZE_NORMAL }}>{this.props.rightLabel}</Text>
+                    <View style={styles.title}>
+                        <Text style={{ ...FONT_BOLD, ...FONT_SIZE_LARGE }}>{this.props.leftLabel}</Text>
+                        <Text style={{ ...FONT_SIZE_NORMAL }}>{this.props.rightLabel}</Text>
+                    </View>
+                    <FlatList
+                        style={{ marginTop: 16 }}
+                        data={this.props.isLoading ? undefined : this.props.data}
+                        renderItem={this._renderItemRestorant}
+                        horizontal={true}
+                        showsHorizontalScrollIndicator={false}
+                        keyExtractor={(item, index) => index.toString()}
+                    ></FlatList>
                 </View>
-                <FlatList
-                    style={{ marginTop: 16 }}
-                    data={this.props.isLoading ? undefined : this.props.data}
-                    renderItem={this._renderItemRestorant}
-                    horizontal={true}
-                    showsHorizontalScrollIndicator={false}
-                    ListEmptyComponent={() => (
-                        <View style={{ flex: 1, flexDirection: 'row' }}>
-                            <View style={{ width: 236, height: 132, borderRadius: 8, backgroundColor: GRAY_COLOR, marginRight: 8 }}>
-                            </View>
-                            <View style={{ width: 236, height: 132, borderRadius: 8, backgroundColor: GRAY_COLOR, marginRight: 8 }}>
-                            </View>
-                            <View style={{ width: 236, height: 132, borderRadius: 8, backgroundColor: GRAY_COLOR, marginRight: 8 }}>
-                            </View>
-                            <View style={{ width: 236, height: 132, borderRadius: 8, backgroundColor: GRAY_COLOR, marginRight: 8 }}>
-                            </View>
-                        </View>
-                    )
-                    }
-                    keyExtractor={(item, index) => index.toString()}
-                ></FlatList>
-            </View>
             )
         }
         return (
